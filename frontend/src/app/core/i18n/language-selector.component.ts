76d08e641daa0db070a7f89c67e8e879cd6b37ca
Luis Martinez <lmartinezocoro@gmail.com>
2026-07-12 18:29:31 +0200

    Sync language selector with active locale

--- frontend/src/app/core/i18n/language-selector.component.ts
+++ frontend/src/app/core/i18n/language-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, effect, inject, viewChild } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { I18nService, SupportedLanguage } from './i18n.service';
 
@@ -8,7 +8,7 @@ import { I18nService, SupportedLanguage } from './i18n.service';
   template: `
     <label class="language-select">
       <span>{{ i18n.t('language.label') }}</span>
-      <select [value]="i18n.language()" (change)="changeLanguage($event)">
+      <select #languageSelect autocomplete="off" [value]="i18n.language()" (change)="changeLanguage($event)">
         <option *ngFor="let language of i18n.languages" [value]="language.code">
           {{ language.label }}
         </option>
@@ -18,6 +18,16 @@ import { I18nService, SupportedLanguage } from './i18n.service';
 })
 export class LanguageSelectorComponent {
   readonly i18n = inject(I18nService);
+  private readonly select = viewChild<ElementRef<HTMLSelectElement>>('languageSelect');
+
+  constructor() {
+    effect(() => {
+      const select = this.select()?.nativeElement;
+      if (select) {
+        select.value = this.i18n.language();
+      }
+    });
+  }
 
   changeLanguage(event: Event): void {
     const target = event.target as HTMLSelectElement;
